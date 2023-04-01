28b093b7fc4eabe0ddd35e3eae4766ac723da02b
Replit user <>
2023-04-01 13:22:48 +0000

    new try

--- arraylist.c
+++ arraylist.c
@@ -59,7 +59,8 @@ void* pop(ArrayList * l, int i){
   }
   if(i>l->size)
     return NULL;
-  if(i<0){
+  
+  if(i<0&&i<=(l->size)*-1){
     int pos = l->size + i;
     for(int j=pos;j<l->size;j++){
       l->data[j]=l->data[j+1];
